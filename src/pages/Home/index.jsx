6ee218356b91bd41373e5e070c2c9131b1469cb8
Hedbert Mathieu <Mathieu.h1@outlook.fr>
2023-06-19 16:49:49 +0200

    màj

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,12 @@
+//* importe la var data du fichier data.js / auto en faisant log(data) -> propose le lien
+
 import logo from './logo.svg';
 import './App.css';
+import { data } from './data';
+
 
 function App() {
+  console.log(data);
   return (
     <div className="App">
       <header className="App-header">
