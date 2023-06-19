1b3258292c04e0434b6a79070312b74ad2b613ca
Hedbert Mathieu <Mathieu.h1@outlook.fr>
2023-06-19 17:48:18 +0200

    sup lignes de code qui créaient des erreurs

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,11 @@
 //* importe la var data du fichier data.js / auto en faisant log(data) -> propose le lien
-
-import logo from './logo.svg';
-import './App.css';
-import { data } from './data';
-
+import { data } from "./data";
 
 function App() {
   console.log(data);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
