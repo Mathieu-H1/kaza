853323686e2cadbee57892749f4febd9e322adeb
Hedbert Mathieu <Mathieu.h1@outlook.fr>
2023-06-28 17:04:53 +0200

    màj

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Header from "./components/Header/header_comp";
 import Home from './pages/Home/home';
 import Logement from './pages/Logement/logement';
 import APropos from './pages/A_propos/a_propos';
-import Header from "./components/Header/header_comp";
 import Footer from "./components/Footer/footer_comp";
 import Error from "./components/Error/error_comp";
 
@@ -15,17 +16,18 @@ const root = createRoot(document.getElementById('root'));
 
 //* path='*' toutes les autres routes / element ce que renvoit le composant (fc°)
 //* incorporer header et footer dans le global car ds ttes les pages
+//! vérifier routes 26/06 
 root.render(
-  <React.StrictMode>
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/logement" element={<Logement />} />
-        <Route path="/a_propos" element={<APropos />} />
-        <Route path='*' element={<Error />} />
-      </Routes>
-      <Footer />
-    </Router>
-  </React.StrictMode>,
+	<React.StrictMode>
+		<Router>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/Logement" element={<Logement />} />
+				<Route path="/A_propos" element={<APropos />} />
+				<Route path='*' element={<Error />} />
+			</Routes>
+			<Footer />
+		</Router>
+	</React.StrictMode>,
 )
