cf1be666729bfb94f84fbd7ea87c09c4411dc66b
Hedbert Mathieu <Mathieu.h1@outlook.fr>
2023-06-28 17:05:33 +0200

    création composant card

---
+++ src/components/Cards/card_comp.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { data } from "../../Data/data";
+import styles from './card.module.css';
+
+function Card() {
+   return (
+      <section className={styles.card}>
+         <div>
+
+
+         </div>
+      </section>
+   )
+}
+
+export default Card
+
+//*
