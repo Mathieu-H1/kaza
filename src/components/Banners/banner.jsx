577b41ac059b907bf3d98942b4f57c9418bf453d
Hedbert Mathieu <Mathieu.h1@outlook.fr>
2023-11-14 14:33:06 +0100

    ajout commentaires  et responsive design + màj css

--- src/components/Banners/banner.jsx
+++ src/components/Banners/banner.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './banner.module.css';
 
+//* composant bannière qui servira pour toutes les bannières des différentes pages
 //* children permet d'ajouter ce que l'on veut changer
 function Banner({ children }) {
    return (
