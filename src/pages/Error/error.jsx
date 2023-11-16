f172911cec5492dc1d7def6ecb508c820b8011bd
Hedbert Mathieu <Mathieu.h1@outlook.fr>
2023-11-16 15:38:58 +0100

    ajout responsive design page erreur

--- src/pages/Error/error.jsx
+++ src/pages/Error/error.jsx
@@ -1,7 +1,7 @@
 //* modifier page index composant error en page erreur_404
 //* pas de composant react car page unique
 
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import styles from './error.module.css';
 
 function Error() {
@@ -21,25 +21,3 @@ function Error() {
 }
 
 export default Error;
-/*
-function Home() {
-   console.log(datas);
-   return (
-      <section className={styles.homePage}>
-         <Banner>
-            <div className={styles.picBanner}> <img className={styles.pictureBanner} src={Mer} alt="côte littoral" /> </div>
-            <div className={styles.textBanner}>Chez vous,{<span className={styles.back}><br /></span>} partout et ailleurs</div>
-         </Banner>
-         <section className={styles.gallery}>
-            <div className={styles.cardsGallery}>
-               {datas.map(logement => {
-                  return (<Card logement={logement} key={logement.id} />)
-               })}
-            </div>
-         </section>
-      </section>
-   );
-}
-
-export default Home;
-*/
